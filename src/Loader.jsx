2627995bb04db8989b5f514f226dd54a01e1d9ba
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-29 06:24:13 +0530

    __

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,21 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect, } from "react";
 import { useValues } from "./GlobalContexts";
+import { useState } from "react";
 
 export default function Loader() {
  const { loading, } = useValues();
-  const loaderRef = useRef();
+  const [display, setdisplay] = useState('none')
 
   useEffect(() => {
-    if (loading) { loaderRef.current.showModal() }
-    else { loaderRef.current.close() }
+    if (loading) { setdisplay('flex') }
+    else { setdisplay('none') }
   }, [loading])
 
   return (
-    <dialog ref={loaderRef} className="modal">
-      <div className="modal-box text-center">
+    <div style={{display}} className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center">
+      <div className="p-2 bg-base-content text-base-100 text-center rounded">
         <p>Fetching pricing data</p>
         <div className=" loading loading-bars"></div>
       </div>
-    </dialog>
+    </div>
   )
 }
