88897947017ebe0d8352d246d717e168dcb5f5c9
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-29 20:41:48 +0530

    __

--- src/GlobalContexts.jsx
+++ src/GlobalContexts.jsx
@@ -4,7 +4,7 @@ const GlobalContexts = createContext();
 
 export const GlobalProvider=({children})=>{
     const [loading, setLoading] = useState(false);
-    const url= import.meta.env.VITE_API_URL
+    const url= import.meta.env.VITE_API_URL || 'http://localhost:4000'
 
     return <GlobalContexts.Provider value={{loading, setLoading,url}}>
         {children}
